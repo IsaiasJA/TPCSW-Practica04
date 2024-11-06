2fb89e2a5d6e3d213e5e617c692a28ed273a73af
IsaiasJA <159166127+IsaiasJA@users.noreply.github.com>
2024-11-05 19:52:56 -0600

    Delete TPCSW-Practica01 directory

--- TPCSW-Practica01/src/main/java/org/uv/tpcsw/practica01/inyeccion/IMensajeID.java
+++
@@ -1,5 +0,0 @@
-package org.uv.tpcsw.practica01.inyeccion;
-
-public interface IMensajeID {
-    public void imprimir();
-}
--- TPCSW-Practica01/src/main/java/org/uv/tpcsw/practica01/inyeccion/MensajeController.java
+++
@@ -1,8 +0,0 @@
-package org.uv.tpcsw.practica01.inyeccion;
-
-public class MensajeController {
-    public void mostrar(IMensajeID mensaje){
-        mensaje.imprimir();
-    }
-    
-}
