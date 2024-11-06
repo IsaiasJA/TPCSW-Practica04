283e5639b1bf920200aff9f4bef5717b5b5a2004
IsaiasJA <isaiasjuareza04@gmail.com>
2024-11-05 19:48:10 -0600

    Subiendo archivos

---
+++ TPCSW-Practica04/src/main/java/com/example/demo/RepositoryEmpleados.java
@@ -0,0 +1,12 @@
+
+package com.example.demo;
+
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ *
+ * @author isaias
+ */
+public interface RepositoryEmpleados extends CrudRepository<Empleado, Long> {
+    
+}
